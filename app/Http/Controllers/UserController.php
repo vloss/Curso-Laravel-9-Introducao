3e8dc449cfa2d8ea8ae347f16bed07a286722704
vloss <viniciusvianaloss@gmail.com>
2022-11-21 12:56:13 -0300

    Criando controller e linkando com as rotas.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    //
+    public function show(User $user){
+
+        //dd($id);
+
+        return $user ;
+    }
 }
