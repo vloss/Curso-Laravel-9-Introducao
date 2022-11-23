c7aaa8dc928a39425214babe29e742ef53a24037
vloss <viniciusvianaloss@gmail.com>
2022-11-23 01:32:31 -0300

    Finalizando layouts com blade template.

--- resources/views/layouts/layout_sistema.blade.php
+++ resources/views/layouts/layout_sistema.blade.php
@@ -4,10 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Curso de Laravel 9 Indrodução</title>
+    <title>@yield('title', 'Titulo Padrão')</title>
 </head>
 <body>
-    <h1>Sistema Curso Laravel 9 Introdução</h1>
+    <h1>@yield('descricao', 'Descrição Padrão')</h1>
+
+    @section('sidebar')
+        <div>
+            <nav>
+                sidebar padrão
+            </nav>
+        </div>
+    
+    @show
 
     @yield('content')
 
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.layout_sistema')
 
+@section('title', 'Listagem de Usuários')
+@section('descricao', 'Listagem de Usuários')
+
+@section('sidebar')
+    <div>
+        <nav>
+            sidebar
+        </nav>
+    </div>
+@endsection
+
 @section('content')
     @foreach ($listagem as $item)
         <p>{{$item->name}}</p><br />
