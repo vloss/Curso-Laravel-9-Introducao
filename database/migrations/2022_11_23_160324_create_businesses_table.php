0a2d37a8637b531edd130449ce502d2d6f011241
vloss <viniciusvianaloss@gmail.com>
2022-11-25 14:39:22 -0300

    Criando Seeders e factories para popular o banco de dados.

--- database/migrations/2022_11_23_160324_create_businesses_table.php
+++ database/migrations/2022_11_23_160324_create_businesses_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('businesses', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->nullable();
+            $table->string('address')->nullable();
+
             $table->timestamps();
         });
     }
